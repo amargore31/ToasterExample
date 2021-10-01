e842d02ca682629ef90e62210d790d8e638d52cd
amar <gore.amar1@gmail.com>
2021-10-01 09:53:01 +0530

    Initial Commit

---
+++ toaster/src/main/java/in/smartbitinfotech/toaster/ToastMessage.java
@@ -0,0 +1,13 @@
+package in.smartbitinfotech.toaster;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class ToastMessage {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message,Toast.LENGTH_SHORT).show();
+
+    }
+}
